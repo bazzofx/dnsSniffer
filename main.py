39ee53ec0d8347401109248392f9e7331719e621
unknown <bazzofx@outlook.com>
2024-11-20 23:32:12 +0000

    added scapy and Virtual env

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import argparse
+from scapy.all import arp
+parser = argparse.ArgumentParser(description="DNS Sniffer")
+parser.add_argument("--targetip", help="Target device you want to watch", required=True)
+parser.add_argument("--iface", help="Interface to use for attack", required=True)
+opts = parser.parse_args()
+
+
+class Device:
+    def __init(sefl):
+        pass
+    def mitm(self):
+        pass
+    def capture(self):
+        pass
+    def watch(self):
+        pass
+
+
+if __name__ == "__main__":
+    device = Device()
+    device.watch()
